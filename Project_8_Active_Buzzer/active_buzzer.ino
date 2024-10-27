659e38116a6d4fc8716cdaf5aa5b30587cd74d01
abdullah-agboola <abdullahagboola001@gmail.com>
2024-10-27 16:56:11 +0100

    Add files via upload

---
+++ Project_8_Active_Buzzer/active_buzzer.ino
@@ -0,0 +1,12 @@
+int buzzerPin = 8;
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(buzzerPin, HIGH);
+    delay(2000);
+    digitalWrite(buzzerPin, LOW);
+    delay(1000);
+}
