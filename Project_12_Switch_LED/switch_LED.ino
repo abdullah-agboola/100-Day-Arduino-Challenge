5a03255d6328e2495274b13c46179ce8bb8e996f
abdullah-agboola <abdullahagboola001@gmail.com>
2024-11-01 10:00:48 +0100

    Add files via upload

---
+++ Project_12_Switch_LED/switch_LED.ino
@@ -0,0 +1,16 @@
+int LED = 10;
+int BUTTON = 4;
+
+void setup(){
+pinMode(LED,OUTPUT);
+pinMode(BUTTON,INPUT);
+}
+
+void loop(){
+if(digitalRead(BUTTON) == HIGH) {
+digitalWrite(LED, HIGH);
+}
+else{
+digitalWrite(LED, LOW);
+}
+}
